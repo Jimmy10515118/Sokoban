02924d2c941ff564cb15a824d26da01f6f0031db
Jimmy10515118 <karta107686@gmail.com>
2017-11-10 13:55:16 +0800

    Jimmy210153

--- httpd/index.js
+++ httpd/index.js
@@ -1,94 +1,81 @@
-/**
- *  @file       index.js
- *  @brief      The entry function of the httpd.
- *  @author     Yiwei Chiao (ywchiao@gmail.com)
- *  @date       10/03/2017 created.
- *  @date       11/10/2017 last modified.
- *  @version    0.1.0
- *  @copyright  MIT, (C) 2017 Yiwei Chiao
- *  @details
- *
- *  The entry function of the httpd.
- */
- //fuck
- 
-'use strict';
-
-let http = require('http');
-
-const routingTable = {
-  '/': {
-      url: '../htdocs/index.html',
-      type: 'text/html'
-    },
-  '/assets/css/styles.css': {
-      url: '../htdocs/assets/css/styles.css',
-      type: 'text/css'
-    },
-  '/assets/png/SokobanClone_byVellidragon.png': {
-      url: '../htdocs/assets/png/SokobanClone_byVellidragon.png',
-      type: 'text/css'
-    },
-};
-
-/**
-  * 利用 http.ServerResponse 物件回傳檔案內容
-  *
-  * @name serve
-  * @function
-  * @param response - http.ServerResponse 物件
-  * @param fname - 要回傳的檔案名
-  * @param datatype - 回傳檔案內容的 Mine-Type
-  * @returns {undefined}
-  */
-let serve = (response, fname, datatype) => {
-  let fs = require('fs');
-
-  fs.readFile(fname, (err, data) => {
-    if (err) {
-      console.log('檔案讀取錯誤');
-    }
-    else {
-      response.writeHead(200, {
-        'Content-Type': datatype
-      });
-
-      response.write(data);
-      response.end();
-    }
-  });
-};
-
-http.createServer((request, response) => {
-  let fs = require('fs');
-
-  let postData = '';
-
-  // 利用 'data' event 消耗掉 data chunk;
-  // 'end' event 才會被 fired
-  request.on('data', (chunk) => {
-    postData += chunk;
-
-    console.log(
-      '接收的 POST data 片段k: [' + chunk + '].'
-    );
-  });
-
-  request.on('end', () => {
-    if (request.url in routingTable) {
-      let obj = routingTable[request.url];
-
-      serve(response, obj.url, obj.type);
-    }
-    else {
-      console.log('未定義的存取: ' + request.url);
-
-      response.end();
-    }
-  });
-}).listen(8088);
-
-// log message to Console
-console.log('伺服器啓動，連線 url:  http://127.0.0.1:8088/');
-
-// index.js
+1. 'use strict';
+2.
+3. let http = require('http');
+4.
+5. const routingTable = {
+6. '/': {
+7. url: '../htdocs/index.html',
+8. type: 'text/html'
+9. },
+10. '/styles.css': {
+11. url: '../htdocs/assets/css/styles.css',
+12. type: 'text/css'
+13. },
+14. '/SokobanClone_byVellidragon.png': {
+15. url: '../htdocs/assets/png/SokobanClone_byVellidragon.png',
+16. type: 'text/css'
+17. },
+18. };
+19.
+20. /**
+21. * 利⽤http.ServerResponse 物件回傳檔案內容
+22. *
+23. * @name serve
+24. * @function
+25. * @param response - http.ServerResponse 物件
+26. * @param fname - 要回傳的檔案名
+27. * @param datatype - 回傳檔案內容的Mine-Type
+28. * @returns {undefined}
+29. */
+30. let serve = (response, fname, datatype) => {
+31. let fs = require('fs');
+32.
+33. fs.readFile(fname, (err, data) => {
+34. if (err) {
+35. console.log(' 檔案讀取錯誤');
+36. }
+37. else {
+38. response.writeHead(200, {
+39. 'Content-Type': datatype
+40. });
+14
+41.
+42. response.write(data);
+43. response.end();
+44. }
+45. });
+46. };
+47.
+48. http.createServer((request, response) => {
+49. let fs = require('fs');
+50.
+51. let postData = '';
+52.
+53. // 利⽤'data' event 消耗掉data chunk;
+54. // 'end' event 才會被fired
+55. request.on('data', (chunk) => {
+56. postData += chunk;
+57.
+58. console.log(
+59. ' 接收的POST data ⽚段k: [' + chunk + '].'
+60. );
+61. });
+62.
+63. request.on('end', () => {
+64. if (request.url in routingTable) {
+65. let obj = routingTable[request.url];
+66.
+67. serve(response, obj.url, obj.type);
+68. }
+69. else {
+70. console.log(' 未定義的存取: ' + request.url);
+71.
+72. response.end();
+73. }
+74. });
+75. }).listen(8088);
+76.
+77. // log message to Console
+78. console.log(' 伺服器啓動，連線url: http://127.0.0.1:8088/');
+79.
+80. // index.js
